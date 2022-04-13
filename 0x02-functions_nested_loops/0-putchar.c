2cf3729e337dbc403ce06d05a7e71c91931ebd6c
josephgreat <josephgreat2003@gmail.com>
2022-04-13 15:02:01 -0700

    printed _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
 #include "main.h"
 
-int main(void) 
+int main(void)
 {
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
 	_putchar('t');
-	_purchar('c');
+	_putchar('c');
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
 
 	return (0);
-}
-
-int _putchar(char word)
-{
-	return (write(1, &word, 1));
 }
