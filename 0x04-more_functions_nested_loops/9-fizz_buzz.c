6be7e29bfb95f90640ca6d338f66e2e1bbedc8bd
josephgreat <josephgreat2003@gmail.com>
2022-04-15 08:37:54 -0700

    Fizz buzz code

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - print numbers 1-100
+ * Fizz if number is a multiple of 3,
+ * Buzz for multiples of 5
+ * FizzBuzz for muliples of 5 and 3
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3) == 0)
+			printf("Fizz");
+		else if ((num % 5) == 0)
+			printf("Buzz");
+		else if ((num % 3) == 0 && (num % 5) == 0)
+			printf("FizzBuzz");
+		else
+			printf("%d", num);
+
+		printf(" ");
+	}
+
+	printf("\n");
+
+	return (0);
+}
