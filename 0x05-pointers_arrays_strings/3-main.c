1f8641a8b7ac52b9579fd8a098ff751283cc237f
josephgreat <josephgreat2003@gmail.com>
2022-04-21 07:51:17 -0700

    fix new line instruction in the _puts function

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -11,6 +11,5 @@ int main(void)
 
 	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
 	_puts(str);
-	_putchar('\n');
 	return (0);
 }
--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -15,4 +15,5 @@ void _puts(char *str)
 		_putchar(*t);
 		t++;
 	}
+	_putchar('\n');
 }
