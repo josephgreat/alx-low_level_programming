4a38585a6445395287ec3fcaf53601bf5c4d96f9
josephgreat <josephgreat2003@gmail.com>
2022-04-22 04:14:09 -0700

    print odd strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - writes the string in stdout
+ * @str: string
+ * Return: nothing
+ */
+
+void puts2(char *str)
+{
+	char *t = str;
+
+	while (*t != '\0')
+	{
+		_putchar(*t);
+		t += 2;
+	}
+	_putchar('\n');
+}
