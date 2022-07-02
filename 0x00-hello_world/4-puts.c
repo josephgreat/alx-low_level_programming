ef7b8de8f3a4cf28978a0f75d5d2001a10f55613
Joseph <josephgreat2003@gmail.com>
2022-07-02 04:21:12 -0700

    My first print with puts()

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - displays a string on stdout
+ *
+ * Return: Always 9 (Success)
+ */
+int main(void)
+{
+	puts("Programming is like building a multiligual puzzle\n");
+
+	return (0);
+}
