bdfb63586bdb1c144c0e20c83fcad5f1f1cec4bd
Joseph <josephgreat2003@gmail.com>
2022-07-04 16:20:45 -0700

    fixed string literal

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	if (i > 5)
 		printf("Last digit of %d is %d and is greater than 5\n", n, i);
 	else if (i == 0)
-		printf("Last digit of %d is %d is 0\n", n, i);
+		printf("Last digit of %d is %d and is 0\n", n, i);
 	else if (i <= 5 && i != 0)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, i);
 	return (0);
