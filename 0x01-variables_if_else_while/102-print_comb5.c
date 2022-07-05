72cb8489648619f684519eaa34dd6606a5d6ba64
Joseph <josephgreat2003@gmail.com>
2022-07-04 18:42:33 -0700

    hectic display of number combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - prints different combination of numbers (2 by 2 digits)
+ * Return: 0
+ */
+
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 99; digit1++)
+	{
+		for (digit2 = 0; digit2 < 100; digit2++)
+		{
+			putchar(digit1 / 10 + '0');
+			putchar(digit1 % 10 + '0');
+			putchar(' ');
+			putchar(digit2 / 10 + '0');
+			putchar(digit2 % 10 + '0');
+
+			if (digit1 == 98 && digit2 == 99)
+				continue;
+
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
