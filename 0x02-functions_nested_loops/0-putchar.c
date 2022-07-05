dd64b6e08c17ce7b1025c2586588b5be4d27bb12
Joseph <josephgreat2003@gmail.com>
2022-07-05 07:09:20 -0700

    print charqcters

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,8 @@
-/**
- * Description:  A file that prints _putchar
- * File: 0-putchar.c
- */
 #include "main.h"
 /**
- * main - writes "_putchar"
+ *main - writes "_putchar"
  *
- * Return: Always 0.
+ *Return: Always 0.
 */
 int main(void)
 {
