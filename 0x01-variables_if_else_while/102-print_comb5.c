121a70c781d89f7d8b9ea9944e9e9204bb0c94ba
Joseph <josephgreat2003@gmail.com>
2022-07-04 18:45:40 -0700

    correction made

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,7 +11,7 @@ int main(void)
 
 	for (digit1 = 0; digit1 < 99; digit1++)
 	{
-		for (digit2 = 0; digit2 < 100; digit2++)
+		for (digit2 = digit1 + 1; digit2 < 100; digit2++)
 		{
 			putchar(digit1 / 10 + '0');
 			putchar(digit1 % 10 + '0');
