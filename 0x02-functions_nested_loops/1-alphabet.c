c473501e1a1527a9780aacba62aa68c1ab662661
Joseph <josephgreat2003@gmail.com>
2022-07-17 16:52:57 -0700

    Clearred error

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,14 @@
-/*alphabet.c*/
 #include "main.h"
-/**
-* print_alphabet - prints the alphabet in lowercase followed by a new line
-*/
-void print_alphabet(void)
 
-	for (char letter = 'a'; letter <= 'z'; letter++)
+/**
+ * print_alphabet - prints the alphabet in lowercase followed by a new line
+ */
+
+void print_alphabet(void)
+{
+	char letter;
+	
+	for (letter = 'a'; letter <= 'z'; letter++)
 	{
 		_putchar(letter);
 	}
