7063debee413ff151d310abf4dcad0c241851484
Joseph <josephgreat2003@gmail.com>
2022-07-17 17:03:28 -0700

    cleared betty issues

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,7 @@
-/**
- * File: 2-print_alphabet_x10.c
- * Author: Joseph
- * Description: renders a function that prints alphabets in lower case 10x
- */
 #include "main.h"
 
 /**
- * print_alphabet_x10: prints alphabets 10x
+ * print_alphabet_x10 - prints alphabets 10x
  */
 void print_alphabet_x10(void)
 {
@@ -18,7 +13,7 @@ void print_alphabet_x10(void)
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
 			_putchar(letter);
-		}	
+		}
 		_putchar('\n');
 	}
 }
