84cc56f664c4a295f450222f5b0b0eb994e8f136
Joseph <josephgreat2003@gmail.com>
2022-07-19 19:22:09 -0700

    searching for a harcater isn't easy

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ * *_strchr - searches for a character in a string and renders it's position
+ * @s: string to be searched
+ * @c: character in search for
+ * Return: c if c is found or NULL if not found
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+
+	if(c == '\0')
+		return (s);
+	return (NULL);
+}
