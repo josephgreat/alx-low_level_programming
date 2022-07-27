07dd07eea969483223a075efe0ba81f577851485
Joseph <josephgreat2003@gmail.com>
2022-07-27 14:42:02 -0700

    updated shell file

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,5 +1,2 @@
 #!/bin/sh
-gcc -c !main.c *.c
-ar -rc liball.a *.o
-ranlib liball.a
-gcc main.c -L. -lall -o main.o
+gcc -c *.c && ar -rc liball.a *.o && ranlib liball.a && gcc main.c -L. -lall -o main.o
