afce31fefa0bc79a300e2ce3675468194b570f13
Joseph <josephgreat2003@gmail.com>
2022-07-29 02:45:09 -0700

    n

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include "main.h"
 /**
  * main - ADD arguments passed
@@ -18,7 +17,7 @@ int main(int argc, char **argv)
 	{
 		for (i = 1; i < argc; i++)
 		{
-			if (atoi(argv[i]) == 0 && !strcmp(argv[i], "0"))
+			if (atoi(argv[i]) == 0)
 			{
 				printf("Error\n");
 				return (1);
