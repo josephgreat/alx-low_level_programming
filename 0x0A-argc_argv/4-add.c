1de6efa3e0183773a8dfa0aa19c5f823ab4fc974
Joseph <josephgreat2003@gmail.com>
2022-07-29 01:30:23 -0700

    add numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "main.h"
+/**
+ * main - ADD arguments passed
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: alwyas 0
+ */
+
+
+int main(int argc, char **argv)
+{
+	int result = 0, i;
+
+	if (argc == 1)
+		printf("0\n");
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			if (atoi(argv[i]) == 0 && strcmp(argv[i], "0"))
+			{
+				printf("Error\n");
+				return (1);
+			}	
+			result += atoi(argv[i]);
+		}
+		printf("%d\n", result);
+	}
+	return (0);
+}
