b0fc7ebc0cc00589d2de35f6fbfb7dd500ad5052
Joseph <josephgreat2003@gmail.com>
2022-07-29 00:27:03 -0700

    mm

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - print the number of argumnets passed
  * @argc: argument count
@@ -9,6 +9,7 @@
 
 int main(int argc, char __attribute__((__unused__)) **argv)
 {
-	printf("%d\n", argc);
+	_putchar('0' + argc);
+	_putchar('\n');
 	return (0);
 }
