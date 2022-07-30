09e3742d57f9d0b61e4689a0b10f8500daef9473
Joseph <josephgreat2003@gmail.com>
2022-07-30 03:22:17 -0700

    cleared betty issues

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * _strconcat - concatenate two strings
+ * str_concat - concatenate two strings
  * @s1: string to be concatenated
  * @s2: string to be concatenated
  * Return: NULL if error, else concatenated string
@@ -13,7 +13,11 @@ char *str_concat(char *s1, char *s2)
 	char *newstr;
 	int s1len = 0, s2len = 0, i = 0, j = 0, len;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2)
+		return (s2);
+	else if (s1 && s2 == NULL)
+		return (s1);
+	else if (s1 == NULL && s2 == NULL)
 		return ("");
 	while (s1[s1len])
 		s1len++;
@@ -28,7 +32,7 @@ char *str_concat(char *s1, char *s2)
 		return (NULL);
 	while (i < len)
 	{
-		if(i < s1len)
+		if (i < s1len)
 			*(newstr + i) = *(s1 + i);
 		else
 		{
