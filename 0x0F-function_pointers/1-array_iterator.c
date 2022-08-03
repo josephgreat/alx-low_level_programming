096bd0af6e0e76f3442612de6b15f6e1cb2ce882
Joseph <josephgreat2003@gmail.com>
2022-08-03 10:13:31 -0700

    array iterator

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+#include <stddef.h>
+/**
+ * array_iterator - iterates an array and call the callback function
+ * @array: array
+ * @size: array length
+ * @action: callback function
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i = 0;
+
+	if (!array || size == 0 || !action)
+		return;
+	
+	while (i < size)
+	{
+		action(array[i]);
+		i++;
+	}
+}
