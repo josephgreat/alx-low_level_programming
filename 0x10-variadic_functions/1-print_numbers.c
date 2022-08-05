a9a567ef9d18517c7be2022ca14edcadf421b044
Joseph <josephgreat2003@gmail.com>
2022-08-05 16:32:42 -0700

    hmm

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,9 +14,6 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list nums_list;
 
-	if (n == 0)
-		return;
-
 	va_start(nums_list, n);
 
 	for (i = 0; i < n; i++)
