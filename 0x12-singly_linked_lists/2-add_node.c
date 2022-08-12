1c03c4a3a39e2910fd1316d0f9889f9189558f7d
Joseph <josephgreat2003@gmail.com>
2022-08-11 17:58:07 -0700

    betty

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,7 +11,7 @@
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node = (list_t*)malloc(sizeof(list_t));
+	list_t *new_node = (list_t *)malloc(sizeof(list_t));
 
 	if (!new_node)
 		return (NULL);
