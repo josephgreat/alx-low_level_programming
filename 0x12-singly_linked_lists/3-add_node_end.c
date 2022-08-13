17eb8518779390da204acaae66bbb9dd1c1a678b
Joseph <josephgreat2003@gmail.com>
2022-08-13 08:31:21 -0700

    made some logic changes

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -12,18 +12,21 @@
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *new_node = (list_t *)malloc(sizeof(list_t));
-	list_t *last = *head;
-	if (!new_node)
+	
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->str = strdup(str);
 	new_node->len = strlen(str);
 	new_node->next = NULL;
-	if (last == NULL)
-		last = new_node;
-	while (last->next != NULL)
-		last = last->next;
+	if ((*head) == NULL)
+	{
+		(*head) = new_node;
+		return ((*head));
+	}
+	while ((*head)->next != NULL)
+		(*head) = (*head)->next;
 
-	last->next = new_node;
-	return (last);
+	(*head)->next = new_node;
+	return ((*head));
 }
