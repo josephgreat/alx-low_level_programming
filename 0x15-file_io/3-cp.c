61d72089f7f2fa5645189ac825e8b4b3ef15d134
Joseph <josephgreat2003@gmail.com>
2022-08-23 06:15:57 -0700

    freed buffer

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -29,11 +29,12 @@ void fd_close(int fd)
  * @fddes: destination fd
  * @fdsrc: destinaton fd
  * @code: file code
+ * @buffer: buffer
  */
 void error_check(int check, char *file, int code,
-		char *errortxt, int fddes, int fdsrc)
+		char *errortxt, int fddes, int fdsrc, char *buffer)
 {
-	if (check == -1)
+	if (check < 0)
 	{
 		dprintf(STDERR_FILENO, "%s %s\n", errortxt, file);
 
@@ -43,8 +44,29 @@ void error_check(int check, char *file, int code,
 		if (fdsrc != -1)
 			fd_close(fdsrc);
 
+		free(buffer);
 		exit(code);
 	}
+}
+
+/**
+ * create_buffer - alloctes memory for buffer
+ * @file: the name of the buffer is storing chars for
+ * Return: buffer
+ */
+char *create_buffer(char *file)
+{
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
 }
 
 /**
@@ -60,36 +82,38 @@ void cp(char *src, char *des)
 	int fdsrc, fddes, nread, nwrite;
 	mode_t file_perm;
 
+	buffer = create_buffer(des);
 	fdsrc = open(src, O_RDONLY);
-	error_check(fdsrc, src, 98, "Error: Can't read from file", -1, fdsrc);
+	error_check(fdsrc, src, 98, "Error: Can't read from file", -1,
+			fdsrc, buffer);
 
 	file_perm = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 	fddes = open(des, O_WRONLY | O_CREAT | O_TRUNC, file_perm);
-	error_check(fddes, des, 99, "Error: Can't write to", fddes, fdsrc);
-
-	buffer = malloc(sizeof(char) * 1024);
-	if (buffer == NULL)
-		fd_close(fdsrc);
+	error_check(fddes, des, 99, "Error: Can't write to", fddes,
+			fdsrc, buffer);
 
 	nread = read(fdsrc, buffer, 1024);
-	error_check(fdsrc, src, 98, "Error: Can't read from file", fddes, fdsrc);
+	error_check(fdsrc, src, 98, "Error: Can't read from file", fddes,
+			fdsrc, buffer);
 
 	while (nread > 0)
 	{
 		nwrite = write(fddes, buffer, nread);
 		if (nwrite != nread)
-		{
-			free(buffer);
 			nwrite = -1;
-		}
-		error_check(fddes, des, 99, "Error: Can't write to", fddes, fdsrc);
+
+		error_check(fddes, des, 99, "Error: Can't write to", fddes,
+				fdsrc, buffer);
 
 		nread = read(fdsrc, buffer, 1024);
-		error_check(fdsrc, src, 98, "Error: Can't read from file", fddes, fdsrc);
+		error_check(fdsrc, src, 98, "Error: Can't read from file", fddes,
+				fdsrc, buffer);
 
 		fddes = open(des, O_WRONLY | O_APPEND);
-		error_check(fddes, des, 99, "Error: Can't write to", fddes, fdsrc);
+		error_check(fddes, des, 99, "Error: Can't write to", fddes,
+				fdsrc, buffer);
 	}
+
 	free(buffer);
 	fd_close(fdsrc);
 	fd_close(fddes);
