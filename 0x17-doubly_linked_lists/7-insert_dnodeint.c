428e85acd221f7ffe6de6dee1105e9be32cea2a3
Joseph <josephgreat2003@gmail.com>
2022-09-29 15:59:57 -0700

    added checks on code

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -28,10 +28,14 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 		return (new_node);
 	}
 
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
 	while (count < idx - 1)
 	{
 		if (!current)
-			return (NULL);
+			return (add_dnodeint_end(h, n));
+		
 		current = current->next;
 		count++;
 	}
