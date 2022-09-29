1a15467b1be534f2518f73a4d7e542d805348c1f
Joseph <josephgreat2003@gmail.com>
2022-09-29 02:57:42 -0700

    length of list

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * dlistint_len - length of linked list
+ * @h: head list
+ * Return: number of nodes in list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	unsigned int listLen = 0;
+	const dlistint_t *current = h;
+
+	while (current->next != NULL)
+	{
+		current = current->next;
+		listLen++;
+	}
+	listLen++;
+	return (listLen);
+}
