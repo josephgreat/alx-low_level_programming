9b741b7bf44c2ddd0ea04112d7dea57a4d0d2eed
Joseph <josephgreat2003@gmail.com>
2022-09-29 03:08:21 -0700

    add newlist to beginning of list

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_dnodeint - adds new node at the beginning f a list
+ * @head: head node
+ * @n: new node integer
+ * Return: NULL if failed else new node
+ */
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_node;
+
+	new_node = malloc(sizeof(dlistint_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
